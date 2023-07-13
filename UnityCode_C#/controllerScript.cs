9fb96e6c392203e10d67a6d472b74614d71e5a80
Owen-Mearns <79281673+Owen-Mearns@users.noreply.github.com>
2023-07-12 23:26:24 -0400

    Update and rename ControllerScript.cs to controllerScript.cs

--- UnityCode_C#/controllerScript.cs
+++ UnityCode_C#/controllerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllerScript : MonoBehaviour
+public class controllerScript : MonoBehaviour
 {
     public GameObject spaceship;
     public Material mat;
